6caf758117b89db87e1655bb582abc21860f515f
crmug <crmug@outlook.com>
2014-12-26 13:26:38 +0800

    test

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -11,7 +11,7 @@ namespace GitTest
         static void Main(string[] args)
         {
             Console.WriteLine("Git Test");
-
+            Console.WriteLine("Another update");
             Console.Read();
         }
     }
